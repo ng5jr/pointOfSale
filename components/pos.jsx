aa289d464ce89f4d608e7f708de69994ed20c4be
Nahuel Giordano <nahuelgiordano@hotmail.com>
2024-04-06 20:12:54 -0300

    Read and write on Sheets

--- components/pos.jsx
+++ components/pos.jsx
@@ -25,41 +25,138 @@ function PointOfSale({ prods }) {
     setTotal(total - itemToRemove.subtotal);
   };
 
+  function Submit(e) {
+    e.preventDefault();
+    const formEle = document.querySelector("form");
+    const formDatab = new FormData(formEle);
+    fetch(
+      "https://script.google.com/macros/s/AKfycby6VkMAQr6Kk2307A8Kmjz2hFHUTWvlcVQYFvgnhuBBXxkyQiRF4RPfs6ba7fzdHEjz/exec",
+      {
+        method: "POST",
+        body: formDatab,
+      }
+    )
+      .then((res) => console.log(res, formDatab))
+
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   return (
-    <>
-      <button onClick={() => setNuevaVenta(!nuevaVenta)}>
-        {nuevaVenta ? "CANCELAR VENTA" : "NUEVA VENTA"}
-      </button>
-      {nuevaVenta && (
-        <div>
-          <h2>INGRESE ORDEN</h2>
-          <ul>
-            {productos.length !== 0 &&
-              productos.map((producto, index) => (
-                <li key={index}>
-                  {producto["producto"]} - {producto["precio"]}
-                  <button onClick={() => handleAddToCart(producto)}>
-                    Add to Cart
+    <div className="App">
+      <>
+        <button onClick={() => setNuevaVenta(!nuevaVenta)}>
+          {nuevaVenta ? "CANCELAR VENTA" : "NUEVA VENTA"}
+        </button>
+        {nuevaVenta && (
+          <div>
+            <h2>INGRESE ORDEN</h2>
+            <ul>
+              {productos.length !== 0 &&
+                productos.map((producto, index) => (
+                  <li key={index}>
+                    {producto["producto"]} - {producto["precio"]}
+                    <button onClick={() => handleAddToCart(producto)}>
+                      Add to Cart
+                    </button>
+                  </li>
+                ))}
+            </ul>
+            <h2>Cart</h2>
+            <form className="form" onSubmit={(e) => Submit(e)}>
+              {venta.map((item, index) => {
+                // Obtener la fecha actual
+                const currentDate = new Date().toLocaleDateString();
+                const currentTime = new Date().toLocaleTimeString();
+
+                return (
+                  <div key={index}>
+                    <input
+                      type="hidden"
+                      name={`Producto_${index}`}
+                      value={item.producto}
+                    />
+                    <input
+                      type="hidden"
+                      name={`Cantidad_${index}`}
+                      value={item.cantidad}
+                    />
+                    {/* <input
+                      type="hidden"
+                      name={`Precio_${index}`}
+                      value={item.precio}
+                    />
+                    <input
+                      type="hidden"
+                      name={`Subtotal_${index}`}
+                      value={item.subtotal}
+                    /> */}
+                    <input
+                      type="hidden"
+                      name={`Fecha_${index}`}
+                      value={currentDate}
+                    />
+                    <input
+                      type="hidden"
+                      name={`Hora_${index}`}
+                      value={currentTime}
+                    />
+                    {item.producto} - {item.cantidad} x {item.precio} = $
+                    {item.subtotal.toFixed(2)}
+                    <button
+                      type="button"
+                      onClick={() => handleRemoveFromCart(index)}
+                    >
+                      Remove
+                    </button>
+                  </div>
+                );
+              })}
+              {/* {venta.map((item, index) => (
+                <div key={index}>
+                  <input
+                    type="hidden"
+                    name="Producto"
+                    value={item["producto"]}
+                  />
+                  <input type="hidden" name="Cantidad" value={item.cantidad} />
+                  {item["producto"]} - {item.cantidad} x {item["precio"]} = $
+                  {item.subtotal.toFixed(2)}
+                  <button
+                    type="button"
+                    onClick={() => handleRemoveFromCart(index)}
+                  >
+                    Remove
                   </button>
-                </li>
-              ))}
-          </ul>
-          <h2>Cart</h2>
-          <ul>
-            {venta.map((item, index) => (
-              <li key={index}>
-                {item.Producto} - {item.cantidad} x {item["precio"]} = $
-                {item.subtotal.toFixed(2)}
-                <button onClick={() => handleRemoveFromCart(index)}>
-                  Remove
-                </button>
-              </li>
-            ))}
-          </ul>
-          <h2>Total: ${total.toFixed(2)}</h2>
-        </div>
-      )}
-    </>
+                </div>
+              ))} */}
+              <input className="submitButton" type="submit" value="submit" />
+            </form>
+            <h2>Total: ${total.toFixed(2)}</h2>
+          </div>
+        )}
+      </>
+
+      {/* <div>
+        <form className="form" onSubmit={(e) => Submit(e)}>
+          <input
+            placeholder="Your Name"
+            id="FirstName"
+            name="FirstName"
+            type="text"
+          />
+          <input
+            placeholder="Your Email"
+            id="LastName"
+            name="LastName"
+            type="text"
+          />
+
+          <input className="submitButton" type="submit" value="submit" />
+        </form>
+      </div> */}
+    </div>
   );
 }
 
